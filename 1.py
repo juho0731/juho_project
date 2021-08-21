e22ebcb1b334077952399ad4a44689aef7078227
juho0731 <88918907+juho0731@users.noreply.github.com>
2021-08-21 13:42:11 +0900

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,46 @@
+a = []
+b = []
+
+b.append(3)
+b.append(4)
+
+print(b[0], b[1])
+#짝수, 홀수
+c = int(input())
+if c % 2 == 0:
+    print("even")
+else:
+      print("odd")
+#별찍기
+for i in range(1, 11):
+  print((11-i)*"*")
+#소수
+count = 0
+d = int(input())
+for i in range(2, d):
+    if d % i == 0:
+       count += 1
+if count == 0:
+    print("prime")
+else:
+    print("Not prime")
+#랜덤한 값10개
+import random
+
+e = []
+for i in range(10):
+    e.append (random.randrange(1, 100))
+print(e)
+
+max_value = e[0]
+for i in range(len(e)):
+
+    if max_value> e[i]:
+        max_value = e[i]
+
+
+#최댓값, 최솟값 찾기
+
+
+
+#랜덤한값 하나 받아서 약수를 리스트에 집어넣서 보여주기
