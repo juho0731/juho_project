d1f011bdd6164ead3bc171ad4d3fa1025e03399f
juho0731 <88918907+juho0731@users.noreply.github.com>
2021-09-18 13:35:51 +0900

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,19 @@
+import random
+
+a = []
+for i in range(100):
+    a.append (random.randrange(1, 100))
+print(a)
+
+b = a[0]
+for i in range(100):
+    if b < a[i]:
+        b = a[i]
+print("maximum" ,b)
+
+c = a[0]
+for i in range(100):
+    if c > a[i]:
+        c = a[i]
+print("minimum",c)
+
