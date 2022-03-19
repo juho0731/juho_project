ed7e0a0eb26bbed8244335b62286ec49d84e198f
juho0731 <88918907+juho0731@users.noreply.github.com>
2022-03-19 13:42:01 +0900

    Add files via upload

---
+++ 함수.py
@@ -0,0 +1,52 @@
+# def print_coin():
+#     for i in range(100):
+#         print("비트코인")
+# print_coin()
+
+# def print_with_smile(문자열):
+#     print(문자열 + ":D")
+# print_with_smile("안녕하세요")
+
+# def print_upper_price(price):
+#     print(price*1.3)
+#
+# def print_sum(a,b):
+#     print(a+b)
+
+# def print_arithmetic_operation(a, b):
+#     print("a", "+", "b", "=", a + b)
+#     print("a", "-", "b" "=", a - b)
+#     print("a", "*", "b" "=",  a * b)
+#     print("a", "/", "b" "=",  a / b)
+
+# def print_max(a,b,c):
+#     max = 0
+#     if max < a:
+#         max = a
+#     if max < b:
+#         max = b
+#     if max < c:
+#         max = c
+#     print(max)
+# def print_reverse(a):
+#     print(a[::-1])
+# print_reverse('asdf')
+
+# def print_score(a):
+#     print(sum(a)/len(a))
+#
+# def print_even(a):
+#     b = []
+#     for i in range(len(a)):
+#         if a[i]%2 == 0:
+#             b.append(a[i])
+#     print(b)
+# print_even ([1, 3, 2, 10, 12, 11, 15])
+
+# def print_keys(a):
+#     for key in a.keys():
+#          print(key)
+
+# def pickup_even(a):
+#     return list(filter(lambda x: x%2 == 0,a))
+
