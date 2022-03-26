6139d3e89415ec5681adab4fd996b4af19d33ec4
juho0731 <88918907+juho0731@users.noreply.github.com>
2022-03-26 13:27:31 +0900

    Add files via upload

---
+++ 정렬.py
@@ -0,0 +1,24 @@
+# list_a = [8, 4, 6, 2, 9, 1, 3, 7, 5]
+# def select_sort(list_a):
+#      n = len(list_a)
+#      for i in range(n):
+#           min = i
+#           for j in range(i+1, n):
+#                if list_a[j] < list_a[min]:
+#                     min = j
+#           list_a[i], list_a[min] = list_a[min], list_a[i]
+#      print(list_a)
+#
+# select_sort(list_a)
+
+
+# list_a = [8, 4, 6, 2, 9, 1, 3, 7, 5]
+#
+# def insertion_sort(list_a):
+#      n= len(list_a)
+#      for i in range(1,n):
+#           for j in range(i,0,-1):
+#                if list_a[j-1] > list_a[j]:
+#                     list_a[j-1], list_a[j] = list_a[j], list_a[j-1]
+#                     print(list_a)
+# insertion_sort(list_a)
