831c25969a804b8e9f1bceff2af54caaf84ca683
juho0731 <88918907+juho0731@users.noreply.github.com>
2022-08-06 13:26:19 +0900

    Update selection sort.py

--- sort/selection sort.py
+++ sort/selection sort.py
@@ -1,4 +1,4 @@
-def selection(a):
+def selection_sort(a):
     for i in range(len(a)):
         k = i
         for j in range(i+1, len(a)):
@@ -7,5 +7,5 @@ def selection(a):
         a[k], a[i] = a[i], a[k]
 
 a = [3, 2, 5, 1, 4]
-selection(a)
+selection_sort(a)
 print(a)
