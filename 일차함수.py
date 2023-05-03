a39ab8938d64c5b0af0383e2510d2a60589a4e52
juho0731 <88918907+juho0731@users.noreply.github.com>
2023-05-04 00:01:53 +0900

    Update 일차함수.py

--- 일차함수.py
+++ 일차함수.py
@@ -3,9 +3,13 @@ import tkinter as tk
 window = tk.Tk()
 window.title("일차함수")
 window.geometry("400x400")
+window.configure(bg='DarkTurquoise')
 entry = tk.Entry(window)
-entry.place(x=50, y=50)
-text1 = tk.Label(window, text="입력값")
-text1.place(x=30, y=50)
-window.mainloop()
+entry.place(x=90, y=50)
+def get_entry_value():
+    print(entry.get())
+button = tk.Button(window, text="입력", command=get_entry_value)
+button.place(x=40, y=50)
 
+
+window.mainloop()
